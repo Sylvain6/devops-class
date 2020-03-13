3ed7caacf4ba8e0698d850443541af6d01d55743
Sylvain Coutrot <s.coutrot.ext_azu@sodexo.com>
2020-03-13 14:47:22 +0100

    adding fucntionavjnsmdflbjnvsdhbdw

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  JSON.stringify(res.send(fs.readFileSync('db.json', {
+    encoding: 'utf-8'
+  })));
+});
+
+router.post("/", function (req, res, next) {
+  res.send(req.body);
 });
 
 module.exports = router;
